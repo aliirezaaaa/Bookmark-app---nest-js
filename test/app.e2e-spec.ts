0393f7c5148f377059af3da3cf52782f7a2cd6d8
Alireza Khorramshekooh <alireza.khorramshekooh@gmail.com>
2025-04-15 16:33:02 +0330

    clean database before e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { Test } from '@nestjs/testing'
 import { AppModule } from '../src/app.module'
 import { INestApplication, ValidationPipe } from '@nestjs/common'
+import { PrismaService } from '../src/prisma/prisma.service'
 
 describe('App e2e', () => {
   let app: INestApplication
+  let prisma: PrismaService
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule]
@@ -13,6 +15,9 @@ describe('App e2e', () => {
       whitelist: true
     }))
     await app.init()
+
+    prisma = app.get(PrismaService)
+    await prisma.cleanDb()
   })
   afterAll(() => {
     app.close()
